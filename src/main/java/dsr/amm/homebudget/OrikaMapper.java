90d829436b7e3577ca34f7c2220f8f9274d4caae
Konstantin Nekrasov <const.nekrasoff@gmail.com>
2018-11-23 19:04:18 +0300

    Introduced Orika into the project

---
+++ src/main/java/dsr/amm/homebudget/OrikaMapper.java
@@ -0,0 +1,8 @@
+package dsr.amm.homebudget;
+
+import ma.glasnost.orika.impl.ConfigurableMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrikaMapper extends ConfigurableMapper {
+}
