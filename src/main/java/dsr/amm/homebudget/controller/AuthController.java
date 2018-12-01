532491c63d9a82fa1d9ab4825debdbc254f71e52
Coderast <Coderast@example.com>
2018-12-01 17:22:23 +0300

    Added interaction between AuthController and AuthService

--- src/main/java/dsr/amm/homebudget/controller/AuthController.java
+++ src/main/java/dsr/amm/homebudget/controller/AuthController.java
@@ -1,6 +1,8 @@
 package dsr.amm.homebudget.controller;
 
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
     @RequestMapping(path="/login", method = RequestMethod.POST)
     public void login(LoginDTO loginDTO) {
-        // FIXME: call the proper service!
+        authService.login(loginDTO);
     }
 
 }
