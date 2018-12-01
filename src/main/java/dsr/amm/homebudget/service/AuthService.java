f1851fc8942bd02ff28ac704ed508aaa453f6d28
Coderast <Coderast@example.com>
2018-12-01 17:24:08 +0300

    Implemented login authentication without session registration

--- src/main/java/dsr/amm/homebudget/service/AuthService.java
+++ src/main/java/dsr/amm/homebudget/service/AuthService.java
@@ -1,13 +1,13 @@
 package dsr.amm.homebudget.service;
 
+import dsr.amm.homebudget.OrikaMapper;
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.data.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -17,10 +17,14 @@ public class AuthService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private OrikaMapper mapper;
+
     public void login(LoginDTO loginDTO) {
-        // FIXME: implement authentication logic
-        // authenticationManager.authenticate()
-        // etc
-        throw new RuntimeException("Not implemented");
+        UsernamePasswordAuthenticationToken token
+                = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
+        authenticationManager.authenticate(token);
+        // TODO: session registration
     }
 }
