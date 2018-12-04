34328fbe3c2d372d0344b6af165c4f78f3cbfd3e
Coderast <Coderast@example.com>
2018-12-04 20:10:25 +0300

    Added unique to column < username > in Person entity

--- src/main/java/dsr/amm/homebudget/data/entity/Person.java
+++ src/main/java/dsr/amm/homebudget/data/entity/Person.java
@@ -18,7 +18,7 @@ public class Person {
 
     @NotNull
     @Length(min = 1, max = 40)
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Length(max = 256)
