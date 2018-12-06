010289845accd7b7410ff77852d3639422cfaf3c
Marina Filina <katerain45@gmail.com>
2018-12-06 20:43:52 +0300

    updated login & added sign up

--- src/main/java/dsr/amm/homebudget/controller/AuthController.java
+++ src/main/java/dsr/amm/homebudget/controller/AuthController.java
@@ -1,6 +1,9 @@
 package dsr.amm.homebudget.controller;
 
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.data.dto.RegistrationDTO;
+import dsr.amm.homebudget.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
     @RequestMapping(path="/login", method = RequestMethod.POST)
     public void login(LoginDTO loginDTO) {
-        // FIXME: call the proper service!
+       authService.login(loginDTO);
+    }
+
+    @RequestMapping(path = "/signup", method = RequestMethod.POST)
+    public void registration(RegistrationDTO registrationDTO){
+        authService.userRegistration(registrationDTO);
     }
 
 }
