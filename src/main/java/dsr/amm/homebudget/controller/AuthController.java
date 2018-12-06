45c7565fb872765747cb7ee5c9ecaba45ede5caf
onceRemastered <olga-avseeva@mail.ru>
2018-12-06 19:02:06 +0300

    Create new package for Principal class. Authentication logic implemented in AuthController and AuthService (I guess)
    Signed-off-by: onceRemastered <olga-avseeva@mail.ru>

--- src/main/java/dsr/amm/homebudget/controller/AuthController.java
+++ src/main/java/dsr/amm/homebudget/controller/AuthController.java
@@ -1,6 +1,8 @@
 package dsr.amm.homebudget.controller;
 
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService service;
+
     @RequestMapping(path="/login", method = RequestMethod.POST)
     public void login(LoginDTO loginDTO) {
-        // FIXME: call the proper service!
+        service.login(loginDTO);
     }
 
 }
--- src/main/java/dsr/amm/homebudget/service/AuthService.java
+++ src/main/java/dsr/amm/homebudget/service/AuthService.java
@@ -1,13 +1,19 @@
 package dsr.amm.homebudget.service;
 
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.data.entity.Person;
+import dsr.amm.homebudget.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -17,10 +23,9 @@ public class AuthService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
     public void login(LoginDTO loginDTO) {
-        // FIXME: implement authentication logic
-        // authenticationManager.authenticate()
-        // etc
-        throw new RuntimeException("Not implemented");
+       authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(),
+               loginDTO.getPassword()));
     }
 }
