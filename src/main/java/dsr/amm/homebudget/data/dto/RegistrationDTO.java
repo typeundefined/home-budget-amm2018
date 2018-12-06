a8692c1bc85c9ef56328bd00d893efcfacf72de7
Marina Filina <katerain45@gmail.com>
2018-12-06 22:46:17 +0300

    added RegistrationDTO

---
+++ src/main/java/dsr/amm/homebudget/data/dto/RegistrationDTO.java
@@ -0,0 +1,38 @@
+package dsr.amm.homebudget.data.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class RegistrationDTO {
+
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private String fullName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+}
