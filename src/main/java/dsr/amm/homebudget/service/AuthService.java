fda738a1726c0ec36fdbcef3a5a1eda00fab3b9f
Marina Filina <katerain45@gmail.com>
2018-12-06 22:47:00 +0300

    added userRegistration and login in AuthService

--- src/main/java/dsr/amm/homebudget/service/AuthService.java
+++ src/main/java/dsr/amm/homebudget/service/AuthService.java
@@ -1,26 +1,81 @@
 package dsr.amm.homebudget.service;
 
+import dsr.amm.homebudget.OrikaMapper;
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import dsr.amm.homebudget.data.dto.RegistrationDTO;
+import dsr.amm.homebudget.data.entity.Person;
+import dsr.amm.homebudget.data.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Collection;
+import java.time.OffsetDateTime;
+
 
 @Service
-public class AuthService {
+public class AuthService{
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private OrikaMapper mapper;
+
+    @Autowired
+    private PersonRepository repository;
+
+    @Transactional
     public void login(LoginDTO loginDTO) {
-        // FIXME: implement authentication logic
-        // authenticationManager.authenticate()
-        // etc
-        throw new RuntimeException("Not implemented");
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authentication);
+        }
+        catch(AuthenticationException e){
+            System.err.println("Failed to authenticate : " + loginDTO.getUsername());
+        }
     }
+
+    public void userRegistration(RegistrationDTO registrationDTO) throws UniqueConditionException {
+        Person person = mapper.map(registrationDTO, Person.class);
+
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        person.setRegisterDate(OffsetDateTime.now());
+        try {
+            this.repository.save(person);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new UniqueConditionException(
+                    "Username '" + person.getUsername() + "' is already in use"
+            );
+        }
+    }
+
+    @ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+    public class UniqueConditionException extends RuntimeException{
+
+        UniqueConditionException(String msg) {
+            super(msg);
+        }
+    }
+
+
+
 }
+
