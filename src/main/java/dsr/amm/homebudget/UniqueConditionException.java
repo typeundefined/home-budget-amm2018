71c3ce47fdc4911d98bb11478faaa64887644f0c
Marina Filina <katerain45@gmail.com>
2018-12-07 11:31:31 +0300

    HttpStatus changed to BAD_REQUEST

--- src/main/java/dsr/amm/homebudget/UniqueConditionException.java
+++ src/main/java/dsr/amm/homebudget/UniqueConditionException.java
@@ -3,7 +3,7 @@ package dsr.amm.homebudget;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE)
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class UniqueConditionException extends RuntimeException{
 
     public UniqueConditionException(String msg) {
