91291ac219a271655fd95faa963523312c5b4cad
Konstantin Nekrasov <konstantin.nekrasov@dsr-corporation.com>
2018-12-07 12:59:13 +0300

    CORS supported

--- src/main/java/dsr/amm/homebudget/controller/AuthController.java
+++ src/main/java/dsr/amm/homebudget/controller/AuthController.java
@@ -1,6 +1,9 @@
 package dsr.amm.homebudget.controller;
 
 import dsr.amm.homebudget.data.dto.LoginDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
+
     @RequestMapping(path="/login", method = RequestMethod.POST)
-    public void login(LoginDTO loginDTO) {
+    public void login(@RequestBody LoginDTO loginDTO) {
+        LOG.debug("Login attempt: {}", loginDTO.getUsername());
         // FIXME: call the proper service!
     }
 
