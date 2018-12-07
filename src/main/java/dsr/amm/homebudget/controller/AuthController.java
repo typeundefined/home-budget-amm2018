5e407f498aa51501948b6567f3a249058f15111f
Marina Filina <katerain45@gmail.com>
2018-12-07 09:38:40 +0300

    fixed json issue in registration method

--- src/main/java/dsr/amm/homebudget/controller/AuthController.java
+++ src/main/java/dsr/amm/homebudget/controller/AuthController.java
@@ -4,6 +4,7 @@ import dsr.amm.homebudget.data.dto.LoginDTO;
 import dsr.amm.homebudget.data.dto.RegistrationDTO;
 import dsr.amm.homebudget.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class AuthController {
     }
 
     @RequestMapping(path = "/signup", method = RequestMethod.POST)
-    public void registration(RegistrationDTO registrationDTO){
+    public void registration(@RequestBody RegistrationDTO registrationDTO){
         authService.userRegistration(registrationDTO);
     }
 
