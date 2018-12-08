244f58befcdbe9381ff63cc69cbe4a1a80a40cf0
Eugene Zubakhin <newzubakhin@gmail.com>
2018-12-09 00:45:03 +0300

    Add missing component annotation

--- src/main/java/dsr/amm/homebudget/util/authentication/PrincipalRetrieverImpl.java
+++ src/main/java/dsr/amm/homebudget/util/authentication/PrincipalRetrieverImpl.java
@@ -2,7 +2,9 @@ package dsr.amm.homebudget.util.authentication;
 
 import dsr.amm.homebudget.service.Principal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PrincipalRetrieverImpl implements PrincipalRetriever{
 
     @Override
