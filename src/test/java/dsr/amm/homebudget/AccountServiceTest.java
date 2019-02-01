b28f9258f235b9314db6bbaf4faa6a83754a02bc
Konstantin Nekrasov <konstantin.nekrasov@dsr-corporation.com>
2019-02-01 13:00:01 +0300

    Tests for deposit and withdrawal

--- src/test/java/dsr/amm/homebudget/AccountServiceTest.java
+++ src/test/java/dsr/amm/homebudget/AccountServiceTest.java
@@ -1,10 +1,7 @@
 package dsr.amm.homebudget;
 
 import dsr.amm.homebudget.controller.exception.NotFoundException;
-import dsr.amm.homebudget.data.dto.AccountNewDTO;
-import dsr.amm.homebudget.data.dto.CurrencyDTO;
-import dsr.amm.homebudget.data.dto.CurrencyIdDTO;
-import dsr.amm.homebudget.data.dto.RegistrationDTO;
+import dsr.amm.homebudget.data.dto.*;
 import dsr.amm.homebudget.data.entity.Account;
 import dsr.amm.homebudget.data.repository.AccountRepository;
 import dsr.amm.homebudget.service.AccountService;
@@ -15,6 +12,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,10 +24,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {HomeBudgetApplication.class, TestConfig.class})
@@ -90,6 +92,68 @@ public class AccountServiceTest {
         AccountNewDTO dto = new AccountNewDTO();
         dto.setCurrency(currId("EUR"));
         accountService.create(dto);
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails("jpetrucci")
+    public void depositWorks() {
+        currencyService.create(currency("RUB"));
+        AccountNewDTO dto = new AccountNewDTO();
+        dto.setCurrency(currId("RUB"));
+
+        AccountDTO acc = accountService.create(dto);
+        accountService.deposit(acc.getId(), deposit(14.0));
+        List<AccountDTO> accList = accountService.getMyAccounts();
+
+        assertEquals(accList.get(0).getCurrentValue(), BigDecimal.valueOf(14.0));
+
+        Page<TransactionDTO> txHistory = accountService.getAccountTransactions(Pageable.unpaged(), acc.getId());
+        assertEquals(txHistory.getTotalElements(), 1L);
+        TransactionDTO tx = txHistory.iterator().next();
+        assertEquals("deposit", tx.getType());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails("jpetrucci")
+    public void witdrawalWorks() {
+        currencyService.create(currency("RUB"));
+        AccountNewDTO dto = new AccountNewDTO();
+        dto.setCurrency(currId("RUB"));
+
+        AccountDTO acc = accountService.create(dto);
+        accountService.deposit(acc.getId(), deposit(20.0));
+        accountService.withdraw(acc.getId(), withdraw(11.0));
+        List<AccountDTO> accList = accountService.getMyAccounts();
+
+        assertEquals(accList.get(0).getCurrentValue(), BigDecimal.valueOf(9.0));
+
+        Page<TransactionDTO> txHistory = accountService.getAccountTransactions(Pageable.unpaged(), acc.getId());
+        assertEquals(txHistory.getTotalElements(), 2L);
+        TransactionDTO tx = txHistory
+                .stream()
+                .filter(t -> t.getType().equals("withdrawal"))
+                .findFirst().get();
+
+        assertEquals("withdrawal", tx.getType());
+        assertTrue(tx instanceof WithdrawalTxDTO);
+    }
+
+    private WithdrawalTxDTO withdraw(double v) {
+        WithdrawalTxDTO dto = new WithdrawalTxDTO();
+        dto.setAmount(BigDecimal.valueOf(v));
+        dto.setCurrency(currId("RUB"));
+        dto.setReason("reason");
+        return dto;
+    }
+
+    private DepositTxDTO deposit(double val) {
+        DepositTxDTO dto = new DepositTxDTO();
+        dto.setCurrency(currId("RUB"));
+        dto.setAmount(BigDecimal.valueOf(val));
+        dto.setReason("reason");
+        return dto;
     }
 
     private CurrencyIdDTO currId(String name) {
